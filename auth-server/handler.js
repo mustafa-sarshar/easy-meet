b6b7538c649d703a9906c157a6cc2c50fbda561d
musto <musti.sar4@gmail.com>
2022-12-09 13:16:32 +0100

    Update the handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,7 +1,7 @@
 "use_strict";
 
 const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+// const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 /**
  * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
