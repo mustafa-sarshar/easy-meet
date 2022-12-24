d863c31c224b465b278b6eb15958d2945b32df16
musto <musti.sar4@gmail.com>
2022-12-24 03:56:37 +0100

    Update bootstrap-related codes

--- src/components/event/index.js
+++ src/components/event/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Card, Accordion } from "react-bootstrap";
 
 import "./styles.css";
+
+const genres = ["JavaScript", "Node", "jQuery", "React", "Angular"];
 
 class Event extends Component {
   constructor(props) {
@@ -20,42 +21,72 @@ class Event extends Component {
 
   render() {
     const { event } = this.props;
+    const genre = genres.filter((genre) =>
+      event.summary.trim().toLowerCase().includes(genre.toLowerCase())
+    );
 
     return (
-      <Card className="event h-100 w-100">
-        <Card.Title>
+      <div className="card event h-100 w-100">
+        <div className="card-header" style={{ fontSize: "0.8rem" }}>
+          <b>*** {genre} ***</b>
+        </div>
+        <div className="card-title">
           <h3 className="event-summary">{event.summary}</h3>
-        </Card.Title>
-        <Card.Body>
+        </div>
+        <div className="card-body">
           <p className="event-start">
             {new Date(event.start.dateTime).toString()}
           </p>
           <p className="event-location">{`@${event.location}`}</p>
-        </Card.Body>
-        <Card.Footer style={{ padding: "0" }}>
-          <Accordion flush>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header className="event-details">
-                About event
-              </Accordion.Header>
-              <Accordion.Body>
-                <p className="event-details__description">
-                  {event.description}
-                </p>
-                <a
-                  className="event-details__htmlLink"
-                  href={event.htmlLink}
-                  target="_blank"
-                  rel="noreferrer"
+        </div>
+
+        <div className="card-footer" style={{ padding: "0" }}>
+          <div
+            className="accordion accordion-flush"
+            id={`accordionEvent_${event.id}`}
+            style={{ width: "100%" }}
+          >
+            <div className="accordion-item">
+              <h2
+                className="accordion-header event-details"
+                id={`headingOneEvent_${event.id}`}
+              >
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapseEvent_${event.id}`}
+                  aria-expanded="false"
+                  aria-controls={`collapseEvent_${event.id}`}
                 >
-                  See the details on Google Calendar
-                </a>
-                <div className="clearfix"></div>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Card.Footer>
-      </Card>
+                  About event
+                </button>
+              </h2>
+              <div
+                id={`collapseEvent_${event.id}`}
+                className="accordion-collapse collapse"
+                aria-labelledby={`#headingOneEvent_${event.id}`}
+                data-bs-parent={`#accordionEvent_${event.id}`}
+              >
+                <div className="accordion-body">
+                  <p className="event-details__description">
+                    {event.description}
+                  </p>
+                  <a
+                    className="event-details__htmlLink"
+                    href={event.htmlLink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    See the details on Google Calendar
+                  </a>
+                  <div className="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/components/events-statistics/index.js
+++ src/components/events-statistics/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tab, Tabs, Accordion } from "react-bootstrap";
+import { Tab, Tabs } from "react-bootstrap";
 
 import BarRechart from "../charts/bar-rechart";
 import PieRechart from "../charts/pie-rechart";
@@ -10,46 +10,103 @@ const EventsStatistics = (props) => {
   const { cityStatisticsData, eventsGenreStatisticsData } = props;
 
   return (
-    <Accordion className="accordion-container">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Statistics</Accordion.Header>
-        <Accordion.Body>
-          <Tabs
-            defaultActiveKey="cityStats"
-            id="statistics"
-            className="mb-5"
-            justify
+    <div className="accordion" id="accordionEventsStatistics">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="#">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseEventsStatistics-1"
+            aria-expanded="false"
+            aria-controls="collapseEventsStatistics-1"
           >
-            <Tab eventKey="cityStats" title="Number of events per city">
-              <BarRechart
-                data={cityStatisticsData.filter(
-                  (data) => data.number !== undefined
-                )}
-                plotData={{
-                  xData: "city",
-                  xLabel: "City",
-                  yData: "number",
-                  yLabel: "Number of Events",
-                }}
-              />
-            </Tab>
-            <Tab eventKey="genreStats" title="Number of events per genre">
-              <PieRechart
-                data={eventsGenreStatisticsData.filter(
-                  (data) => data.number !== 0
-                )}
-                plotData={{
-                  xData: "genre",
-                  xLabel: "Genre",
-                  yData: "number",
-                  yLabel: "Number of Events",
-                }}
-              />
-            </Tab>
-          </Tabs>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+            Statistics
+          </button>
+        </h2>
+        <div
+          id="collapseEventsStatistics-1"
+          className="accordion-collapse collapse"
+          aria-labelledby="#headingOneEventsStatistics"
+          data-bs-parent="#accordionEventsStatistics"
+        >
+          <div className="accordion-body">
+            <ul
+              className="nav nav-tabs mb-5 justify-content-around"
+              id="eventsStatisticsTabs"
+              role="tablist"
+            >
+              <li className="nav-item" role="presentation">
+                <button
+                  className="nav-link active"
+                  id="nCityEvents-tab"
+                  data-bs-toggle="tab"
+                  data-bs-target="#nCityEvents"
+                  type="button"
+                  role="tab"
+                  aria-controls="nCityEvents"
+                  aria-selected="true"
+                >
+                  Number of events per city
+                </button>
+              </li>
+              <li className="nav-item" role="presentation">
+                <button
+                  className="nav-link"
+                  id="eventsGenre-tab"
+                  data-bs-toggle="tab"
+                  data-bs-target="#eventsGenre"
+                  type="button"
+                  role="tab"
+                  aria-controls="eventsGenre"
+                  aria-selected="false"
+                >
+                  Number of events per genre
+                </button>
+              </li>
+            </ul>
+            <div className="tab-content" id="eventsStatisticsTabsContent">
+              <div
+                className="tab-pane fade show active"
+                id="nCityEvents"
+                role="tabpanel"
+                aria-labelledby="#nCityEvents-tab"
+              >
+                <BarRechart
+                  data={cityStatisticsData.filter(
+                    (data) => data.number !== undefined
+                  )}
+                  plotData={{
+                    xData: "city",
+                    xLabel: "City",
+                    yData: "number",
+                    yLabel: "Number of Events",
+                  }}
+                />
+              </div>
+              <div
+                className="tab-pane fade"
+                id="eventsGenre"
+                role="tabpanel"
+                aria-labelledby="eventsGenre-tab"
+              >
+                <PieRechart
+                  data={eventsGenreStatisticsData.filter(
+                    (data) => data.number !== 0
+                  )}
+                  plotData={{
+                    xData: "genre",
+                    xLabel: "Genre",
+                    yData: "number",
+                    yLabel: "Number of Events",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
